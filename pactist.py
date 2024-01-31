978b3f3e1280e4f51223fcd538a43d3d202952a2
Psalms_Martial <109070870+PuffMartial@users.noreply.github.com>
2024-01-31 14:49:26 +0200

    Add files via upload

---
+++ pactist.py
@@ -0,0 +1,8 @@
+username = ["Nira","Kevin","sigebenga"]
+password = ("aBc123","p@zzworD66","Olwe2")
+login_date = ["24/1/2024","27/12024","27/12/2014"]
+
+users = zip(username,password,login_date)
+
+for i in users:
+    print(i)
